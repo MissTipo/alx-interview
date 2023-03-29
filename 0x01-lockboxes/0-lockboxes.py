91f65870dea6ee95dffc7d07eb2df11bf58426a9
MissTipo <dorine.a.tipo@gmail.com>
2023-03-29 07:08:46 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+'''
+n = number of boxes = length of array = len(boxes)
+unlocked = [0]
+
+'''
+#!/usr/bin/python3
+"""Lockboxes"""
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    n = len(boxes)
+    unlocked_boxes = [0]
+    for box_num, box in enumerate(boxes):
+        for key in box:
+            if key < n and key not in unlocked_boxes and key != box_num:
+                unlocked_boxes.append(key)
+    return len(unlocked_boxes) == n
