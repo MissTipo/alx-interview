644e4819718d0f7fd5f99a275ca7372094afdae7
MissTipo <dorine.a.tipo@gmail.com>
2023-03-29 11:07:07 +0300

    fix requirement

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Lockboxes"""
+
+
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened"""
     n = len(boxes)
     unlocked_boxes = [0]
     for box_num, box in enumerate(boxes):
         for key in box:
-            if not box:
-                continue
             if key < n and key not in unlocked_boxes and key != box_num:
                 unlocked_boxes.append(key)
     return len(unlocked_boxes) == n
