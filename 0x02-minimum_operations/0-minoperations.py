4588c33cfdec74e7344672641eaf88e06f158520
MissTipo <dorine.a.tipo@gmail.com>
2023-04-05 13:37:40 +0300

    fix inline comments

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,16 +7,16 @@ def minOperations(n):
     alculates the fewest number of operations needed to
     result in exactly n H characters
     '''
-    if n <= 1: # If n is impossible to achieve
+    if n <= 1:  # If n is impossible to achieve
         return 0
-    num_operations = 0 # number of operations required
-    i = 2 # initialize i to the smallest prime factor
+    num_operations = 0  # number of operations required
+    i = 2  # initialize i to the smallest prime factor
     # Divide n by its prime factors, starting with the smallest prime factor
     while i <= n:
         if n % i == 0:
             n //= i
             num_operations += i
-            i = 2 # reset the value of i
+            i = 2  # reset the value of i
         else:
             i += 1
     return num_operations
