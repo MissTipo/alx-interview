4953d3c85da52bd1c1c496973a105d6d9a9c0635
MissTipo <dorine.a.tipo@gmail.com>
2023-04-11 15:29:37 +0300

    remove the helper function

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 '''Minimum Operations'''
-import math
-
-
-def is_prime(n):
-    """
-    Helper function to determine whether a number is prime
-    """
-    if n < 2:
-        return False
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            return False
-    return True
-
 
 def minOperations(n):
     '''
