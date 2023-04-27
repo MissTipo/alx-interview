db325e547ef3c006f6ca677e7508f971b56ec235
MissTipo <dorine.a.tipo@gmail.com>
2023-04-27 06:23:16 +0300

    different approach

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,56 +1,75 @@
 #!/usr/bin/python3
-""""""
+"""N queens"""
 import sys
 
-
+# Check if the number of arguments is correct
 if len(sys.argv) != 2:
-    print('Usage: nqueens N')
+    print("Usage: nqueens N")
     sys.exit(1)
 
-N = sys.argv[1]
-if N is not isinstance(N, int):
-    print('N must be a number')
+# Check if N is a number
+try:
+    n = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
     sys.exit(1)
 
-if int(N) < 4:
-    print('N must be at least 4')
+# Check if N is at least 4
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+# Check if N is even and at least 6
+if n % 2 == 0 and n < 6:
+    print("N must be at least 6 if it is even")
+    sys.exit(1)
+
+# Check if N is odd and at least 5
+if n % 2 == 1 and n < 5:
+    print("N must be at least 5 if it is odd")
     sys.exit(1)
 
 
 # A helper function to check if a queen can be placed in a given position
-def is_safe(board, row, column):
-    """A function to check if a queen can be placed in a given position"""
-    N = len(board)
-    # Check if there is already a queen in the same column
-    for i in range(N):
-        if [i][column] == 'Q':
-            return False
-        # Check if there is already a queen in the same row
-        if [i][row] == 'Q':
-            return False
-        # Check if there is already a queen in the same upper diagonal
-        for i, j in enumerate(range(row, -1, -1)):
-            if board[i][j] == 'Q':
-                return False
-        # Check if there is already a queen in the same lower diagonal
-        for i, j in enumerate(range(row, N, 1)):
-            if board[i][j] == 'Q':
-                return False
-        return True
-
-
-def nqueens(board, row):
-    """Solve the nqueens"""
+def is_valid(board, row, col):
+    """check if a queen can be placed in a given position"""
     n = len(board)
+    # Check if there is already a queen in the same column
+    for i in range(n):
+        if board[i][col] == "Q":
+            return False
+    # Check if there is already a queen in the same row
+    for j in range(n):
+        if board[row][j] == "Q":
+            return False
+    # Check if there is already a queen in the same diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == "Q":
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == "Q":
+            return False
+    return True
+
+# A recursive function to solve the N Queens problem
+def solve(board, row):
+    """ solve the N Queens problem"""
+    n = len(board)
+    # Base case: all queens have been placed
     if row == n:
-        print(' '.join(board[i]) for i in range(n))
+        print(" ".join(board[i]) for i in range(n))
         return
+    # Try to place a queen in each column of the current row
     for col in range(n):
-        if is_safe(board, row, col):
+        if is_valid(board, row, col):
             board[row][col] = "Q"
-            nqueens(board, row + 1)
+            solve(board, row + 1)
             board[row][col] = "."
 
 
-board = [["."] * N for _ in range(N)]
-nqueens(board, 0)
+# Initialize the board
+board = [["."] * n for _ in range(n)]
+
+# Call the solve function with the initial board and the first row
+solve(board, 0)
+
