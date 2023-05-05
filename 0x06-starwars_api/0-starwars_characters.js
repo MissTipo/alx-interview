023fac60525d60dbe7769829b96aa3cf284fcee9
MissTipo <dorine.a.tipo@gmail.com>
2023-05-05 05:17:23 +0300

    starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+import request from 'request-promise';
+
+const printCharacters = async (movieId) => {
+  const movieUrl = 'https://swapi-api.alx-tools.com/api/films/${movieId}/';
+  try {
+    const movieData = await request.get(movieUr);
+    const characterUrls = JSON.parse(movieData).characters
+
+    const characterPromises = characterUrls.map((url) => request.get(url));
+    const characterResponse = await Promise.all(characterPromises);
+
+    const characters = characterResponses.map((response) => JSON.parse(response))
+    characters.forEach((character) => console.log(character.name));
+  } catch (error) {
+    console.error('Error retrieving character data: ${error}');
+  }
+};
+
+const movieId = process.argv[2];
+printCharacters(movieId);
+
+
