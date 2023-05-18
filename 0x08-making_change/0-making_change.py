0b2640cffc563455a63c7a415d351b2a06953d16
MissTipo <dorine.a.tipo@gmail.com>
2023-05-18 08:05:00 +0300

    efficiency check

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,24 +7,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    if min(coins) > total:
-        return -1
-
-    if sum(coins) == total:
-        return len(coins)
-
-    if total in coins:
-        return 1
 
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
         for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+            if dp[amount - coin] != float('inf'):
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    if dp[total] == float('inf'):
-        return -1
-
-    return dp[total]
+    return dp[total] if dp[total] != float('inf') else -1
 
